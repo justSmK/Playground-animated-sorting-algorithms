206f8735f8325ddcb0d0b30bf3b2102156abfe99
justSmK <semko431@gmail.com>
2022-08-07 14:23:25 +0300

    Refactored

--- PASA.playground/Pages/Bubble sort.xcplaygroundpage/Contents.swift
+++ PASA.playground/Pages/Bubble sort.xcplaygroundpage/Contents.swift
@@ -1,23 +1,28 @@
 import Foundation
 
-var array = randomArray(length: 10, max: 30)
+var array = randomArray(length: 30, max: 50)
 print(array)
 var arrayView = ArrayStackView()
 
-//var isSorted: Bool
-
-let size = array.count
-
-for i in 0..<size {
-    let pass = (size - 1) - i
-    for j in 0..<pass {
-        let key = array[j]
-        
-        if key > array[j + 1] {
-            array.swapAt(j, j + 1)
-            arrayView.update(with: array)
+func bubbleSort(array: inout [Int]) -> [Int] {
+    
+    let size = array.count
+    
+    for i in 0..<size {
+        let pass = (size - 1) - i
+        for j in 0..<pass {
+            let key = array[j]
+            
+            if key > array[j + 1] {
+                array.swapAt(j, j + 1)
+                arrayView.update(with: array)
+            }
         }
     }
+    
+    return array
 }
+
+bubbleSort(array: &array)
 
 assert(array == array.sorted())
--- PASA.playground/Pages/Counting Sort.xcplaygroundpage/Contents.swift
+++ PASA.playground/Pages/Counting Sort.xcplaygroundpage/Contents.swift
@@ -4,26 +4,28 @@ var array = randomArray(length: 30, max: 50)
 print(array)
 var arrayView = ArrayStackView()
 
-let maxVal = array.max()!
-//    let size = array.count
-let m = maxVal + 1
-
-var container = Array(repeating: 0, count: m)
-
-for j in array {
-    container[j] += 1
-}
-
-var i = 0
-
-for a in 0..<m {
-    print(a)
-    print(container[a])
-    for _ in 0..<container[a] {
-        array[i] = a
-        i += 1
-        arrayView.update(with: array)
+func countingSort(array: inout [Int]) -> [Int] {
+    let maxVal = array.max()!
+    let m = maxVal + 1
+    
+    var container = Array(repeating: 0, count: m)
+    
+    for j in array {
+        container[j] += 1
     }
+    
+    var i = 0
+    
+    for a in 0..<m {
+        for _ in 0..<container[a] {
+            array[i] = a
+            i += 1
+            arrayView.update(with: array)
+        }
+    }
+    return array
 }
+
+countingSort(array: &array)
 
 assert(array == array.sorted())
